34a8b933de934e87708e48dae789fb9f27b8bf3d
jianyong <1694163757@qq.com>
2019-01-04 21:25:07 +0800

    修复了小箭头的bug

--- js/index.js
+++ js/index.js
@@ -14,51 +14,56 @@ var contentMain=document.querySelector('.contentMain');
 var content=document.querySelector('.content');
 var contentHeight=content.offsetHeight;
 var index=0;
-
+var timer=null;
 
     document.onmousewheel=wheel;
 document.addEventListener('DOMMouseScroll',wheel);
 function wheel(event) {
     event = event || window.event;
 
-    var flag = '';
-    if (event.wheelDelta) {
-        //ie/chrome
-        if (event.wheelDelta > 0) {
-            flag = 'up';
-            
-        } else {
-            flag = 'down';
+      var flag = '';
+      if (event.wheelDelta) {
+          //ie/chrome
+          if (event.wheelDelta > 0) {
+              flag = 'up';
+
+          } else {
+              flag = 'down';
 
 
-        }
-    } else if (event.detail) {
-        //firefox
-        if (event.detail < 0) {
-            flag = 'up';
-        } else {
-            flag = 'down'
-        }
-    }
+          }
+      } else if (event.detail) {
+          //firefox
+          if (event.detail < 0) {
+              flag = 'up';
+          } else {
+              flag = 'down'
+          }
+      }
 
-    switch (flag) {
-        case 'up' :
-            if(index>0){
-                index --;
-                contentMain.style.top=-contentHeight*index+'px';
-                arrow.style.left=liNodes[index].getBoundingClientRect().left+liNodes[index].offsetWidth/2-arrow.offsetWidth/2+'px';
-            };
-            break;
-        case 'down' :
-            if(index<4){
-                index ++;
-                contentMain.style.top=-contentHeight*index+'px';
-                arrow.style.left=liNodes[index].getBoundingClientRect().left+liNodes[index].offsetWidth/2-arrow.offsetWidth/2+'px';
-            }
-            break;
-    }
+      switch (flag) {
+          case 'up' :
+              if(index>0){
+                  index --;
+                  contentMain.style.top=-contentHeight*index+'px';
+                  arrow.style.left=liNodes[index].getBoundingClientRect().left+liNodes[index].offsetWidth/2-arrow.offsetWidth/2+'px';
+              };
+              break;
+          case 'down' :
+              if(index<4){
+                  index ++;
+                  contentMain.style.top=-contentHeight*index+'px';
+                  arrow.style.left=liNodes[index].getBoundingClientRect().left+liNodes[index].offsetWidth/2-arrow.offsetWidth/2+'px';
+              }
+              break;
+      }
 
-    //禁止默认行为
+      //禁止默认行为
+
+
     event.preventDefault && event.preventDefault();
     return false;
+}
+window.onresize=function () {
+    arrow.style.left=liNodes[index].getBoundingClientRect().left+liNodes[index].offsetWidth/2-arrow.offsetWidth/2+'px';
 }
